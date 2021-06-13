a69891671765adf44c603c8c941eb362ff957917
nguyenminhsang2004 <67231653+nguyenminhsang2004@users.noreply.github.com>
2021-06-14 00:20:12 +0700

    ghep fe, be va run build fe

---
+++ API/application/controllers/Home.php
@@ -0,0 +1,74 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+	
+class Home extends CI_Controller {
+
+	public function __construct()
+	{
+		header('Access-Control-Allow-Origin: http://18.219.140.198');
+    	header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+    	header('Access-Control-Allow-Headers: Origin, content-type, X-Auth-Token,  Access-Control-Allow-Origin');
+    	header('Content-Type: application/json');
+
+		parent::__construct();
+	}
+
+	public function index()
+	{
+		
+	}
+
+	public function compileCode()
+	{
+		$program = $this->input->post('program');
+		$language = $this->input->post('language');
+		$result = '';
+
+		if($language == 1){
+			$myFile = fopen('csharp/Program.cs', 'w+');
+			if(fwrite($myFile, $program) == false){
+				echo 0;
+			}
+			else{
+				exec('docker rm csharpcompile');
+				// exec('docker run -it -d -v d:/Server/htdocs/API/csharp:/data --name csharpcompile ubuntu:18.04');
+				exec('docker run -it -d -v /var/www/html/API/csharp:/data --name csharpcompile ubuntu:18.04');
+				$result = exec('docker exec csharpcompile bash /data/compile.sh');
+			}
+			
+		}
+
+		if($language == 2){
+			$myFile = fopen('java/Main.java', 'w+');
+			if(fwrite($myFile, $program) == false){
+				echo 0;
+			}
+			else{
+				exec('docker rm javacompile');
+				// exec('docker run -it -d -v d:/Server/htdocs/API/java:/data --name javacompile ubuntu:18.04');
+				exec('docker run -it -d -v /var/www/html/API/java:/data --name javacompile ubuntu:18.04');
+				$result = exec('docker exec javacompile bash /data/compile.sh');
+			}
+		}
+
+
+		if($language == 3){
+			$myFile = fopen('python/Main.py', 'w+');
+			if(fwrite($myFile, $program) == false){
+				echo 0;
+			}
+			else{
+				exec('docker rm pythoncompile');
+				// exec('docker run -it -d -v d:/Server/htdocs/API/python:/data --name pythoncompile ubuntu:18.04');
+				exec('docker run -it -d -v /var/www/html/API/python:/data --name pythoncompile ubuntu:18.04');
+				$result = exec('docker exec pythoncompile bash /data/compile.sh');
+				
+			}
+		}
+
+		echo json_encode($result);
+	}
+
+}
+
+/* End of file Home.php */
+/* Location: ./application/controllers/Home.php */	
---
+++ API/csharp/Program.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Program
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine(conghaiso(11,21));
+    }
+
+    static int conghaiso(int a, int b){
+        return a   b;
+    }
+  }
+}
